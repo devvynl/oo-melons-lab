f746ca0eded48493fd75ef39eca8820b41b774d6
Devvyn <devvyn@gmail.com>
2021-08-07 20:03:14 +0000

    updated methods

--- oo-practice-melons/harvest.py
+++ oo-practice-melons/harvest.py
@@ -9,21 +9,35 @@ class MelonType(object):
     def __init__(self, code, first_harvest, color, is_seedless, is_bestseller, 
                  name):
         """Initialize a melon."""
+        self.code = code 
+        self.first_harvest = first_harvest
+        self.color = color
+        self.is_seedless = is_seedless
+        self.is_bestseller = is_bestseller
+        self.name = name 
 
         self.pairings = []
+        print(f'I am {self.name}')
 
         # Fill in the rest
 
     def add_pairing(self, pairing):
         """Add a food pairing to the instance's pairings list."""
+        self.pairing = pairing 
+        pairings.append(pairing)
+        print(f'Pairing list is now {self.pairings}')
 
         # Fill in the rest
 
     def update_code(self, new_code):
         """Replace the reporting code with the new_code."""
-
+        code = super().update_code()
+        self.code = new_code
+        print(f'The code is now {self.code}')
         # Fill in the rest
 
+# melon1 = MelonType('yw', 'Field 2', 7, None, None, 'Melon 1')
+# print(melon1)
 
 def make_melon_types():
     """Returns a list of current melon types."""
